bb627ae19b0a91389e0b8c17415c74739b34fb0c
Ganesh-Ekhe <ganeshekhe8@gmail.com>
2025-03-28 15:07:32 +0530

    Fix: Added allowedHosts in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
-  }
+  },
+  preview: {
+    allowedHosts: ['ganesh-ekhe-portfolio.onrender.com'],
+    port: 10000,
+  },
 })
